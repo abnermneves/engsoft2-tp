6cc9293e0ac391b35052db90055964681bf497f2
Denilson Martins <denilsonmvs@gmail.com>
2024-05-23 22:00:21 -0300

    feat: :sparkles: Update e edit

--- imports/ui/recipes/RecipeEdit.tsx
+++ imports/ui/recipes/RecipeEdit.tsx
@@ -1,7 +1,132 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Meteor } from "meteor/meteor";
+import { useTracker } from "meteor/react-meteor-data";
+import { useNavigate, useParams } from "react-router-dom";
+import { Ingredient, Recipe } from "../../api/collections/recipe/recipe";
 
 export const RecipeEdit: React.FC = () => {
+    const navigate = useNavigate();
+    const userId = useTracker(() => Meteor.userId());
+    const { id } = useParams();
+
+    useEffect(() => {
+        if(!userId) {
+            alert("Você deve estar logado para criar uma receita");
+            navigate("/");
+        }
+    }, []);
+
+
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+    const [steps, setSteps] = useState<string[]>([]);
+
+    useEffect(() => {
+        if(id) {
+            Meteor.call("recipe.getOne", id, (e: Meteor.Error, result: Recipe) => {
+                if(e) {
+                    alert(e);
+                    navigate(-1);
+                    return;
+                }
+
+                setName(result.name);
+                setIngredients(result.ingredients);
+                setSteps(result.steps);
+            });
+        }
+    }, []);
+
+    const submit = () => {
+        const callback = (e: Meteor.Error, res: any) => {
+            if(e) {
+                alert(e);
+                return;
+            }
+
+            if(res !== 1) {
+                alert("Não foi possível salvar.");
+                return;
+            }
+
+            alert("Receita salva com sucesso.");
+            navigate(-1);
+        };
+
+        if(id) {
+            Meteor.call("recipe.edit", {
+                _id: id,
+                name, ingredients, steps,
+                createdBy: userId,
+            }, callback);
+        } else {
+            Meteor.call("recipe.create", {
+                name, ingredients, steps,
+                createdBy: userId,
+            }, callback);
+        }
+    };
+
     return <div>
-        
+        <div>
+            <label>Nome:</label>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+        </div>
+
+        <h3>Ingredientes:</h3>
+        {ingredients.map((ingredient, idx) => <div key={idx.toString()}>
+            <label>Quantidade:</label>
+            <input type="text" value={ingredient.amount} onChange={e => {
+                let newIngredients = [...ingredients];
+                newIngredients[idx].amount = e.target.value;
+                setIngredients(newIngredients);
+            }} />
+            <label>Ingrediente:</label>
+            <input type="text" value={ingredient.name} onChange={e => {
+                let newIngredients = [...ingredients];
+                newIngredients[idx].name = e.target.value;
+                setIngredients(newIngredients);
+            }}/>
+            <button onClick={() => {
+                let newIngredients = [...ingredients];
+                newIngredients.splice(idx, 1);
+                setIngredients(newIngredients);
+            }}>Remover</button>
+        </div>)}
+
+        <div>
+            <button onClick={() => {
+                const newIngredients = [...ingredients, {name: '', amount: ''}];
+                setIngredients(newIngredients);
+            }}>Adicionar Ingrediente</button>
+        </div>
+
+
+        <h3>Modo de preparo</h3>
+        {steps.map((step, idx) => <div key={idx.toString()}>
+            <label>Passo {idx + 1}:</label>
+            <input type="text" value={step} onChange={e => {
+                let newSteps = [...steps];
+                newSteps[idx] = e.target.value;
+                setSteps(newSteps);
+            }}/>
+            <button onClick={() => {
+                let newSteps = [...steps];
+                newSteps.splice(idx, 1);
+                setSteps(newSteps);
+            }}>Remover</button>
+        </div>)}
+
+        <div>
+            <button onClick={() => {
+                const newSteps = [...steps, ''];
+                setSteps(newSteps);
+            }}>Adicionar passo</button>
+        </div>
+
+        <div>
+            <button onClick={submit}>Salvar</button>
+            <button onClick={() => navigate(-1)}>Cancelar</button>
+        </div>
     </div>;
 };
--- imports/ui/recipes/RecipeView.tsx
+++ imports/ui/recipes/RecipeView.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Meteor } from "meteor/meteor";
+import { Recipe, Ingredient } from "/imports/api/collections/recipe/recipe";
+
 
 export const RecipeView: React.FC = () => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+    const [steps, setSteps] = useState<string[]>([]);
+
+    useEffect(() => {
+        if(id) {
+            Meteor.call("recipe.getOne", id, (e: Meteor.Error, result: Recipe) => {
+                if(e) {
+                    alert(e);
+                    navigate(-1);
+                    return;
+                }
+
+                setName(result.name);
+                setIngredients(result.ingredients);
+                setSteps(result.steps);
+            });
+        }
+    }, []);
+
     return <div>
-        
+        <div>
+            <h1>{name}</h1>
+        </div>
+
+        <h3>Ingredientes:</h3>
+        <ol>
+            {ingredients.map((ingredient, idx) => (
+                <li key={idx.toString()}>
+                    <span>{ingredient.amount}: {ingredient.name}</span>
+                </li>
+            ))}
+        </ol>
+
+        <h3>Modo de preparo</h3>
+        <ol>
+            {steps.map((step, idx) => (
+                <li key={idx.toString()}>
+                    <span>{step}</span>
+                </li>
+            ))}
+        </ol>
     </div>;
 };
