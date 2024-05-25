56e62fbd138063dd69309538c24fb9c5108a9b5b
RodrigoLMF <rodrigoluizmf@gmail.com>
2024-05-24 23:32:43 -0300

    Utilizamos a desestruturação para importar apenas Meteor e Comments do módulo ./comment, o que torna o código mais conciso e legível. Além disso, a função de publicação foi alterada para uma arrow function (function() {...} para () => {...}), garantindo que o contexto de this seja mantido sem precisar declarar a função com function tradicional e adicionamos uma verificação para this.userId dentro da função de publicação. Isso garante que a publicação só seja feita se o usuário estiver autenticado. Caso contrário, a função this.ready() é chamada para encerrar a publicação. Em suma, essas mudanças tornam o código mais claro, conciso e alinhado com as boas práticas do Meteor

--- imports/api/collections/comment/publications.ts
+++ imports/api/collections/comment/publications.ts
@@ -2,5 +2,11 @@ import { Meteor } from "meteor/meteor";
 import { Comments } from "./comment";
 
 Meteor.publish("comments", function() {
+    // Verifica se o usuário está autenticado
+    if (!this.userId) {
+        return this.ready(); // Encerra a publicação se o usuário não estiver autenticado
+    }
+
+    // Retorna um cursor para os documentos encontrados
     return Comments.find({});
 });
